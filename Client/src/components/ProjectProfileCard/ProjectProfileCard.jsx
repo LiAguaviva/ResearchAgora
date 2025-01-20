041d22c7add80ff4cb0e69ca422401b26055ddf4
fran <user@users-MacBook-Pro.local>
2025-01-20 21:00:29 +0100

    See all projects in the profile

--- Client/src/components/ProjectProfileCard/ProjectProfileCard.jsx
+++ Client/src/components/ProjectProfileCard/ProjectProfileCard.jsx
@@ -3,10 +3,10 @@ import { AgoraContext } from '../../context/ContextProvider'
 import projectDefaultIMG from '../../assets/imgs/lab1.jpg'
 import './ProjectProfileCard.css'
 
-export const ProjectProfileCard = () => {
+export const ProjectProfileCard = ({elem}) => {
 
-  const {user, project} = useContext(AgoraContext)
-
+  const {user} = useContext(AgoraContext)
+  console.log(elem)
   return (
     <div className='projectProfileCard'>
        <div className='profileProjectImg'>
@@ -17,8 +17,8 @@ export const ProjectProfileCard = () => {
           />
         </div>
         <div className='text'>
-        <h4 className='projectName'>project name</h4>
-        <p className='projectName'>Estado</p>
+        <h4 className='projectName'>{elem.project_title}</h4>
+        <p className='projectName'>{elem.project_status}</p>
         </div>
     </div>
   )
